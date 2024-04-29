2241185a7150d5c9457daa0474ac14c139201d1a
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2024-04-29 15:52:40 +0200

    js examples

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+console.log("I am " + 19 + " years old");
+console.log(15 + 16);
+
+let x = 5;
+let y = 6;
+let z = x + y;
+
+document.getElementById('math').innerHTML = "The value of z is " + z;
